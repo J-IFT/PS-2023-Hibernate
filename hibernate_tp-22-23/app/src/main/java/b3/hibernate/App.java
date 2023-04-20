1d1ed047f213a1f3c6c98c7f05ea583862b7f302
J-IFT <juliette.infanti@gmail.com>
2023-04-20 18:22:04 +0200

    ON PEUT AJOUTER MODIFIER SUPPRIMER ET LISTER LES PRODUITS ET CA MARCHE AVEC LA BDD PUTAIN

--- hibernate_tp-22-23/app/src/main/java/b3/hibernate/App.java
+++ hibernate_tp-22-23/app/src/main/java/b3/hibernate/App.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Scanner;
-
+import org.hibernate.query.Query;
 import b3.hibernate.model.Products;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -86,12 +86,86 @@ public class App {
         }
     }
     private static void updateProduct(){
-        System.out.println("On modifie");
+        System.out.println("Modification d'un produit");
+        System.out.println("-------------------------");
+        System.out.println("Entrez l'ID du produit à modifier (pour le savoir, il faut prendre l'ordre chronologique dans lequel vous avez créé vos produits. par exemple : j'ai créé patate / radis / pizza, alors dans l'ordre ça donne 1 / 2 / 3) :");
+        int productId = scanner.nextInt();
+        scanner.nextLine(); // consommer la ligne vide laissée par nextInt()
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Products products = session.get(Products.class, productId);
+            if (products != null) {
+                System.out.println("Entrez le nouveau nom (ou appuyez sur entrée pour ne pas le modifier) :");
+                String nom = scanner.nextLine();
+                if (!nom.isEmpty()) {
+                    products.setNom(nom);
+                }
+                //System.out.println("Entrez le nouveau code (ou appuyez sur entrée pour ne pas le modifier) :");
+                //String code = scanner.nextLine();
+                //if (!code.isEmpty()) {
+                //    products.setCode(code);
+                //}
+                System.out.println("Entrez la nouvelle quantité (ou appuyez sur entrée pour ne pas la modifier) :");
+                String quantiteStr = scanner.nextLine();
+                if (!quantiteStr.isEmpty()) {
+                    int quantite = Integer.parseInt(quantiteStr);
+                    products.setQuantite(quantite);
+                }
+                System.out.println("Entrez la nouvelle date de péremption (ou appuyez sur entrée pour ne pas la modifier, format: yyyy-mm-dd) :");
+                String datePeremptionStr = scanner.nextLine();
+                if (!datePeremptionStr.isEmpty()) {
+                    LocalDate datePeremption = LocalDate.parse(datePeremptionStr);
+                    products.setDatePeremption(datePeremption);
+                }
+                session.update(products);
+                transaction.commit();
+                System.out.println("Le produit a été modifié avec succès !");
+            } else {
+                System.out.println("Le produit avec l'ID " + productId + " n'existe pas.");
+            }
+        } catch (Exception e) {
+            System.out.println("Une erreur s'est produite lors de la modification du produit : " + e.getMessage());
+        }
     }
     private static void deleteProduct(){
-        System.out.println("On supprime");
+        System.out.println("Suppression d'un produit");
+        System.out.println("-----------------------");
+        System.out.println("Entrez l'ID du produit à supprimer (pour le savoir, il faut prendre l'ordre chronologique dans lequel vous avez créé vos produits. par exemple : j'ai créé patate / radis / pizza, alors dans l'ordre ça donne 1 / 2 / 3)");
+        int productId = scanner.nextInt();
+        scanner.nextLine(); // consommer la ligne vide laissée par nextInt()
+
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Products products = session.get(Products.class, productId);
+            if (products != null) {
+                session.delete(products);
+                transaction.commit();
+                System.out.println("Le produit a été supprimé avec succès !");
+            } else {
+                System.out.println("Le produit avec l'ID " + productId + " n'existe pas.");
+            }
+        } catch (Exception e) {
+            System.out.println("Une erreur s'est produite lors de la suppression du produit : " + e.getMessage());
+        }
     }
     private static void listProducts(){
-        System.out.println("On regarde");
+        System.out.println("Liste des produits :");
+        System.out.println("--------------------");
+
+        try (Session session = sessionFactory.openSession()) {
+            Query<Products> query = session.createQuery("from Products", Products.class);
+            List<Products> productsList = query.list();
+
+            for (Products products : productsList) {
+                System.out.println("Nom : " + products.getNom());
+                System.out.println("Code : " + products.getCode());
+                System.out.println("Quantité : " + products.getQuantite());
+                System.out.println("Date de péremption : " + products.getDatePeremption());
+                System.out.println("----------------------------------");
+            }
+        } catch (Exception e) {
+            System.out.println("Une erreur s'est produite lors de la récupération des produits : " + e.getMessage());
+        }
     }
 }
